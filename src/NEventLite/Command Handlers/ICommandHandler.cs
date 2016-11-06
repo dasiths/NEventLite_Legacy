61f8ee1ed75954d8d19ad90d5e8df3e8162ef86d
Dasith Wijesiriwardena <dasiths@hotmail.com>
2016-11-07 03:51:12 +1100

    Updated to latest version 3:50 AM 07-Nov-16

---
+++ src/NEventLite/Command Handlers/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using NEventLite.Commands;
+
+namespace NEventLite.Command_Handlers
+{
+    public interface ICommandHandler<T> where T:ICommand
+    {
+        int Handle(T command);
+    }
+}
---
+++ src/NEventLite/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NEventLite.Commands
+{
+    public class Command:ICommand
+    {
+        public Guid Id { get; private set; }
+        public Guid AggregateId { get; }
+        public int TargetVersion { get; private set; }
+
+        public Command(Guid id, Guid aggregateId, int targetVersion)
+        {
+            Id = id;
+            AggregateId = aggregateId;
+            TargetVersion = targetVersion;
+        }
+    }
+}
